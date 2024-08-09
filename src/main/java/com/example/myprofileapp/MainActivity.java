c1b425a0cfea7cc9b4df14fc062f419cd2e02dd0
468SiliconWP445 <hotlap8878@gmail.com>
2024-08-09 18:31:59 +0100

    Update MainActivity.java

--- src/main/java/com/example/myprofileapp/MainActivity.java
+++ src/main/java/com/example/myprofileapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myprofileapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Method to open the first YouTube channel
+    public void openYouTubeChannel1(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UC1HsAi6pok0NJEf9_3KaSRA"));
+        startActivity(intent);
+    }
+
+    // Method to open the Minecraft YouTube channel
+    public void openYouTubeChannel2(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UC6Wj7Q94-VU0TVgYM2zErrg"));
+        startActivity(intent);
+    }
+
+    // Method to open the Instagram page
+    public void openInstagram(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/180wsox/"));
+        startActivity(intent);
+    }
+
+    // Method to open the X (formerly Twitter) account
+    public void openX(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://x.com/180w_soxlamp"));
+        startActivity(intent);
+    }
+
+    // Method to open the Sutton Lighting page
+    public void openSuttonLighting(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://180wsox.github.io/180wsox.com/suttonlighting.html"));
+        startActivity(intent);
     }
 }
